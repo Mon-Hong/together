afd35cb0aba7ba65b955f210cb00d105caf75e52
Borwonlak Phothong <78490883+Mon-Hong@users.noreply.github.com>
2023-01-10 08:34:32 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace X
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, num3, x;
+
+
+            Console.Write(" number1 : ");
+            num1 = double.Parse(Console.ReadLine());
+            Console.Write(" number2 : ");
+            num2 = double.Parse(Console.ReadLine());
+            Console.Write(" number3 : ");
+            num3 = double.Parse(Console.ReadLine());
+
+            x = (num1 + num2 + num3) / 3;
+            Console.WriteLine("Mean = {0}" , x);
+
+        }
+    }
+}
